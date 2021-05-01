f61f26e0fe92da2e1b38c1fb7e109bb17e187db7
helen <bluesky6529@gmail.com>
2021-05-01 22:04:38 +0800

    complete aliyun lego example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lego-cert
+
+https://gist.github.com/Dviejopomata/1a9641505ef96b126277a75ad5684e99
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-acme/lego/challenge/dns01"
+	"github.com/go-acme/lego/v4/certcrypto"
+	"github.com/go-acme/lego/v4/certificate"
+	"github.com/go-acme/lego/v4/lego"
+	"github.com/go-acme/lego/v4/providers/dns/alidns"
+	"github.com/go-acme/lego/v4/registration"
+)
+
+// You'll need a user or account type that implements acme.User
+type MyUser struct {
+	Email        string
+	Registration *registration.Resource
+	key          crypto.PrivateKey
+}
+
+func (u *MyUser) GetEmail() string {
+	return u.Email
+}
+func (u MyUser) GetRegistration() *registration.Resource {
+	return u.Registration
+}
+func (u *MyUser) GetPrivateKey() crypto.PrivateKey {
+	return u.key
+}
+
+func main() {
+
+	// Create a user. New accounts need an email and private key to start.
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	myUser := MyUser{
+		Email: "**************",
+		key:   privateKey,
+	}
+
+	config := lego.NewConfig(&myUser)
+
+	// This CA URL is configured for a local dev instance of Boulder running in Docker in a VM.
+	config.CADirURL = "https://acme-staging-v02.api.letsencrypt.org/directory"
+	config.Certificate.KeyType = certcrypto.RSA2048
+
+	// A client facilitates communication with the CA server.
+	client, err := lego.NewClient(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// We specify an HTTP port of 5002 and an TLS port of 5001 on all interfaces
+	// because we aren't running as root and can't bind a listener to port 80 and 443
+	// (used later when we attempt to pass challenges). Keep in mind that you still
+	// need to proxy challenge traffic to port 5002 and 5001.
+
+	// Config is used to configure the creation of the DNSProvider.
+	aliconfig := alidns.Config{
+		APIKey:             "**********",
+		SecretKey:          "**********",
+		RegionID:           "cn-hangzhou",
+		PropagationTimeout: dns01.DefaultPropagationTimeout,
+		PollingInterval:    dns01.DefaultPollingInterval,
+		TTL:                600,
+		HTTPTimeout:        10 * time.Second,
+	}
+
+	p, err := alidns.NewDNSProviderConfig(&aliconfig)
+	err = client.Challenge.SetDNS01Provider(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// New users will need to register
+	reg, err := client.Registration.Register(registration.RegisterOptions{TermsOfServiceAgreed: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+	myUser.Registration = reg
+
+	request := certificate.ObtainRequest{
+		Domains: []string{"******"},
+		Bundle:  true,
+	}
+	certificates, err := client.Certificate.Obtain(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Each certificate comes back with the cert bytes, the bytes of the client's
+	// private key, and a certificate URL. SAVE THESE TO DISK.
+	fmt.Printf("%#v\n", certificates)
+
+	f, _ := os.Create("./" + request.Domains[0] + ".key")
+	f.Write(certificates.PrivateKey)
+	f.Close()
+
+	f1, _ := os.Create("./" + request.Domains[0] + ".crt")
+	f1.Write(certificates.Certificate)
+	f1.Close()
+	// ... all done.
+}
